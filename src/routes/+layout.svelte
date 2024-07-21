715c393d346930271cd9f3073f1e2fe2c44aba13
MNTadros <bobnakhla@gmail.com>
2024-07-20 21:42:13 -0400

    added layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav class="bg-gray-800 p-4 text-white flex justify-between">
+  <div class="flex space-x-4">
+    <a href="/" class="hover:text-gray-300">Home</a>
+    <a href="/contact" class="hover:text-gray-300">Contact</a>
+    <a href="/projects" class="hover:text-gray-300">Projects</a>
+  </div>
+</nav>
+
+<main class="container mx-auto p-4">
+  <slot></slot>
+</main>
