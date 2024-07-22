450fb2094045defe8613bdc9794e806f57fd4565
MNTadros <bobnakhla@gmail.com>
2024-07-22 15:32:09 -0400

    changed how faq questions move around

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     extend: {
       keyframes: {
         move: {
-          '0%, 100%': { transform: 'translateY(0) translateX(0)' },
-          '25%': { transform: 'translateY(-20px) translateX(20px)' },
-          '50%': { transform: 'translateY(20px) translateX(20px)' },
-          '75%': { transform: 'translateY(20px) translateX(-20px)' },
+          '0%': { transform: 'translateY(0) translateX(0)' },
+          '20%': { transform: 'translateY(-10px) translateX(10px)' },
+          '40%': { transform: 'translateY(10px) translateX(10px)' },
+          '60%': { transform: 'translateY(10px) translateX(-10px)' },
+          '80%': { transform: 'translateY(-10px) translateX(-10px)' },
+          '100%': { transform: 'translateY(0) translateX(0)' },
         }
       },
       animation: {
